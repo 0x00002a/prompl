bc7b472dd3667c7cf9ec21eb7d3eed30a306b864
Natasha England-Elbro <ashenglandelbro@protonmail.com>
2023-05-04 14:08:08 +0100

    feat: add git branch

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::{
+    fs::File,
+    io::Read,
+    path::PathBuf,
+    process::{Command, Stdio},
+};
+
 use colours::Colour;
 use component::Component;
 use rand::{seq::SliceRandom, thread_rng};
@@ -12,20 +19,50 @@ fn path_comp() -> Component {
     Component::text(path_sel.to_owned())
         .style(Style::new().fg(Colour::Gray100).bg(Colour::Gray50).bold())
 }
+fn git_comp() -> Option<Component> {
+    let mut root = Command::new("git")
+        .arg("rev-parse")
+        .arg("--show-toplevel")
+        .stderr(Stdio::null())
+        .output()
+        .ok()?
+        .stdout;
+    if root[root.len() - 1] as char == '\n' {
+        root.pop();
+    }
+    let root = PathBuf::from(String::from_utf8(root).ok()?);
+    let mut head = File::open(root.join(".git").join("HEAD")).ok()?;
+    let mut headref = String::new();
+    head.read_to_string(&mut headref).ok()?;
+    let mut branch = headref.split("/").last()?.to_owned();
+    branch.pop();
+    Some(Component::text(branch).style(Style::new().bg(Colour::SkyBlue1).fg(Colour::Gray3).bold()))
+}
 
 fn components() -> Vec<Component> {
-    vec![path_comp()]
+    let mut comps = vec![path_comp()];
+    if let Some(c) = git_comp() {
+        comps.push(c);
+    }
+    comps
 }
 fn join_comps(comps: &[Component], sep: &str) -> String {
-    comps.iter().fold(String::new(), |s, c| {
+    let mut out = Vec::new();
+    for n in 0..comps.len() {
+        let c = &comps[n];
         let r = c.clone().render();
         let mut sep_style = c.get_style().clone().no_bg();
         if let Some(bg) = c.get_style().get_bg() {
             sep_style = sep_style.fg(*bg);
         }
+        if let Some(bg) = comps.get(n + 1).and_then(|c| c.get_style().get_bg()) {
+            sep_style = sep_style.bg(*bg);
+        }
         let sep = Component::text(sep.to_owned()).style(sep_style);
-        s + &r + &sep.render()
-    })
+        out.push(r);
+        out.push(sep.render());
+    }
+    out.join("")
 }
 const POWERLINE_SEP: &str = "";
 const PROMPT_CHARS: [char; 11] = ['€', '#', '$', 'λ', '!', ':', '?', '\\', '/', '«', '*'];
